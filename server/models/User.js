5091e2eb2b078cd9acfe5655df215c615b0beaf8
chris <chrisishere@gmail.com>
2021-01-13 15:13:24 -0800

    [wip] karry
    - cleanup and karry theme tweaks

--- server/models/User.js
+++ server/models/User.js
@@ -145,9 +145,6 @@ userSchema.statics.getRoleName =UserService.getRoleName;
 
 userSchema.methods.validatePassword = async function(pw) {
   const valid = await bcrypt.compare(pw, this.password);
-  const hash = await bcrypt.hash(pw, 10);
-  console.log(hash);
-  debugger;
   return valid;
 };
 
